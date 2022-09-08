cde582b724d4f1724cc609a0d9f2fd542ae6c0bf
yugansh soni <yuganshsoni8@gmail.com>
2022-09-08 22:52:03 +0530

    adding multiplication in branch multiply

---
+++ multiplication.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a ;
+    cout << " ";
+    cin >> b;
+
+    cout << "multiplication of two numbers is : " << a*b;
+}
