d43f90a8dd3a03ec72705b7839cd261e2cd33bed
yugansh soni <yuganshsoni8@gmail.com>
2022-09-10 19:53:21 +0530

    new program to add two numbers

--- sum.cpp
+++ sum.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int a,b;
-    cout << "enter two numbers : ";
+    cout << "enter two numbers to add them :";
     cin >> a >> b;
-    cout << "\nsum of two numbers is : " << a+b;
+    cout << "\n the sum of two numbers is : "<< a+b;   
 }
